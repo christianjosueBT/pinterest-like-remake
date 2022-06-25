fa9146e8eb05102570325341346ef4d6b1a61462
christianjosuebt <christianjosuebt@hotmail.com>
2022-06-25 10:37:29 -0600

    trying to see if I can work on desktop

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-import { Uri, port } from '../config.js';
+// hello!
+import { Uri, port } from '../config.js'
 
-import server from './server.js';
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import server from './server.js'
+import { MongoClient, ServerApiVersion } from 'mongodb'
 
-import CoffeeShopsDAO from './dao/coffeeshopsDAO.js';
-import UsersDAO from './dao/usersDAO.js';
+import CoffeeShopsDAO from './dao/coffeeshopsDAO.js'
+import UsersDAO from './dao/usersDAO.js'
 
 // connecting to mongodb using the mongodb driver!
 MongoClient.connect(Uri, {
@@ -15,14 +16,14 @@ MongoClient.connect(Uri, {
   wtimeoutMS: 2000,
 })
   .catch(err => {
-    console.error(err.stack);
-    process.exit(1);
+    console.error(err.stack)
+    process.exit(1)
   })
   .then(async client => {
-    await CoffeeShopsDAO.injectDB(client);
-    await UsersDAO.injectDB(client);
+    await CoffeeShopsDAO.injectDB(client)
+    await UsersDAO.injectDB(client)
 
     server.listen(port, () => {
-      console.log(`listening on port ${port}`);
-    });
-  });
+      console.log(`listening on port ${port}`)
+    })
+  })
